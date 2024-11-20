bdbe03d85708a63ebb95919a40fe6cc177c4f8dc
omerfarukgulhan <omerfarukgulhan@outlook.com>
2024-11-20 19:18:20 +0300

    new validation messages and event entity updated

--- src/main/java/com/ofg/event/model/entity/Event.java
+++ src/main/java/com/ofg/event/model/entity/Event.java
@@ -1,4 +1,51 @@
 package com.ofg.event.model.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "events")
+@Data
 public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "created_by", nullable = false)
+    private User user;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private String location;
+
+    @Column(name = "event_image", nullable = false)
+    private String eventImage = "default.png";
+
+    @Column(nullable = false)
+    private LocalDateTime dateTime;
+
+    @Column(nullable = false)
+    private int maxCapacity;
+
+    @Column(nullable = false)
+    private int numberOfReviews = 0;
+
+    @Column(nullable = false)
+    private double averageRating = 0.0;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
+    @OneToMany(mappedBy = "event", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Review> reviews;
 }
